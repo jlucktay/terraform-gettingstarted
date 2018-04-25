001d5a25a708c0ac8f376a51dc8dc0a538a948da
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-04-25 17:04:36 +0100

    Tuned up bash options

--- tag_killdate.sh
+++ tag_killdate.sh
@@ -1,1 +1,6 @@
-export TF_VAR_tag_killdate=$(gdate '+%d/%m/%Y' -d "+3 days")
+#!/usr/local/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+TF_VAR_tag_killdate=$(gdate '+%d/%m/%Y' -d "+3 days")
+export TF_VAR_tag_killdate
